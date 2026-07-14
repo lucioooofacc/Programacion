934f0ff7d2f223d70146c1cb5d064d13e02c0656
luciooofacc <mfacchin40@gmail.com>
2026-07-13 22:20:23 -0300

    Agregamos clase mago y guerrero

--- ParcialSemestre/Personajes.java
+++ ParcialSemestre/Personajes.java
@@ -4,10 +4,10 @@ class Personaje {
     private int salud;
     private int nivel;
 
-    public Personaje(String nombre, int salud, int nivel){
+    public Personaje(String nombre, int salud){
         this.nombre = nombre;
         this.salud = salud;
-        this.nivel = nivel;
+        this.nivel = 1;
     }
 
     public void atacar(){
@@ -25,5 +25,47 @@ class Personaje {
     public int getSalud(){
         return salud;
     }
+    public void setSalud(int salud){
+        this.salud = salud;
+    }
 
+    public int getNivel(){
+        return nivel;
+    }
+    public void setNivel(int nivel){
+        this.nivel = nivel;
+    }
+}
+
+class Guerrero extends Personaje {
+    private int fuerza;
+    private int defensa;
+    public Guerrero(String nombre, int salud, int fuerza, int defensa){
+        super(nombre, salud);
+        this.fuerza = fuerza;
+        this.defensa = defensa;
+    }
+
+    public void usarEspada(){
+        System.out.println(getNombre() + "esta usando la espada");
+    }
+    public void gritarDesafio(){
+        System.out.println(getNombre() + "esta gritando desafio");
+    }
+    public void gritarGuerra(){
+        System.out.println(getNombre() + "esta gritando guerra");
+    }
+}
+
+class Mago extends Personaje {
+    private int mana;
+    private int inteligencia;
+public Mago(String nombre, int salud, int mana, int inteligencia){
+      super(nombre, salud);
+         this.mana = mana;
+         this.inteligencia = inteligencia;
+    }
+    public void recuperarMana(){
+
+    }
 }
